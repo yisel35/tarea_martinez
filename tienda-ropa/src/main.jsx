7ed3d5e93b67e25f443103124519f280930bbae2
yisel35 <kimberlynyisel35@gmail.com>
2025-05-29 00:00:09 -0300

    version000.8

--- tienda-ropa/src/main.jsx
+++ tienda-ropa/src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 import ContextProvider from './context/ContextProvider.jsx'
 
