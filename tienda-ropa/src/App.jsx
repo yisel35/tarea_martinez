cca31661ab9126762df1c7b228fc64e447445ca2
yisel35 <kimberlynyisel35@gmail.com>
2025-05-28 23:41:32 -0300

    version0000.7

--- tienda-ropa/src/App.jsx
+++ tienda-ropa/src/App.jsx
@@ -3,15 +3,21 @@ import Header from './components/Header'
 import ItemListContainer from './components/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer'
 import Checkout from './pages/Checkout'
+import Cart from './pages/Cart'
+import About from './pages/About'
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<ItemListContainer greeting="¡Bienvenido a la tienda de ropa!" />} />
-        <Route path="/detalle/:id" element={<ItemDetailContainer />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path='/' element={<ItemListContainer greeting="¡Bienvenido a la tienda de ropa!" />} />
+        <Route path='/category/:category' element={<ItemListContainer />} />
+        <Route path='/detalle/:id' element={<ItemDetailContainer />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='*' element={<h1>Error 404 - Page not found</h1>} />
       </Routes>
     </>
   )
--- tienda-ropa/src/main.jsx
+++ tienda-ropa/src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
+import ContextProvider from './context/ContextProvider.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <ContextProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </ContextProvider>
+  </BrowserRouter>
 )
